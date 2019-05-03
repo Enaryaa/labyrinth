ca20d73fb3167e7bcc50376db078b9bd5e67b3b0
lecharti <romain0.lechartier@etu.u-pec.fr>
2019-05-03 15:57:29 +0200

    ajout lecture/sauvegarde

--- test_romain/Case.java
+++ test_romain/Case.java
@@ -4,14 +4,14 @@ import java.awt.*;
 public class Case extends JPanel
 {
 	private Grille grille;
-	private char index;
+	private int index;
 	private Color couleur;
 	private char etat;
 	private int particularité;
 	private boolean thesee;
 	private boolean sortie;
 
-	public Case(char ind, char e, int p, Grille g)
+	public Case(int ind, char e, int p, Grille g)
 	{
 		super();
 		this.grille = g;
@@ -56,7 +56,7 @@ public class Case extends JPanel
 		return this.etat;
 	}
 
-	public char indexCase()
+	public int indexCase()
 	{
 		return this.index;
 	}
